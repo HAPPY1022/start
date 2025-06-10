25e1e35aadd68a1b1cdcafa6a238122344fcd336
kiwi <pikumin11718091@outlook.jp>
2025-06-10 13:35:42 +0900

    Update janken(Rock-Paper-scissors!).py
    japanese old game!
    Let me know if you get an error!

--- janken(Rock-Paper-scissors!).py
+++ janken(Rock-Paper-scissors!).py
@@ -6,13 +6,13 @@ while True:
   if Player not in hands:
     print("Please enter a valid hands!")
     continue
-  computer = random.choice(hand)
+  computer = random.choice(hands)
   print(f"The computer Played{computer}!")
   if Player == computer:
     print("It's a tie! Let's play again!")
-  elif((player == "rock" and computer == "scissors") or 
-       (player == "scissors" and computer == "paper") or
-       (player == "paper" and computer == "rock")):
+  elif((Player == "rock" and computer == "scissors") or 
+       (Player == "scissors" and computer == "paper") or
+       (Player == "paper" and computer == "rock")):
          print("You win! Congratulations!")
          break
   elif Player == computer:
